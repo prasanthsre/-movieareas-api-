fb0a70e770c5c6343165424072ad0d9d462065d3
prasanthsre <prasanthre2298@gmail.com>
2025-06-28 10:13:50 +0530

    rootstucture

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ app.get("/", (req, res) => {
     res.json({ msg: "Hello Students" });
 });
 
+//CRUD functionality of movies
+
+//R= for READ
+app.get("/movies",()=>{
+
+})
+//C=for CREATE
+app.post("/movies",()=>{
+    
+})
+//U=for UPDATE
+app.put("/movies/:id",()=>{
+    
+})
+//D=for DELETE
+app.delete("/movies/:id",()=>{
+    
+})
 app.listen(PORT, () => {
     console.log(`The server is running at http://localhost:${PORT}`);
 });
