615e7ca7e742d5709d18870fa5981d1776c0ad26
prasanthsre <prasanthre2298@gmail.com>
2025-06-28 10:06:25 +0530

    feat:app structure

---
+++ main.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 6969;
+
+app.get("/", (req, res) => {
+    res.json({ msg: "Hello Students" });
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is running at http://localhost:${PORT}`);
+});
